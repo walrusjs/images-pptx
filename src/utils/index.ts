acd979c40e6b5c8c010b053e735ac1039bf8c8d8
wangxingkang <156148958@qq.com>
2021-11-08 11:39:35 +0800

    feat: 防止拷贝目录不存在报错

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,20 @@
-import { copy } from 'fs-extra'
+import { copy, lstatSync } from 'fs-extra'
+
+/**
+ * 判断是否为目录
+ * @param name
+ */
+export function isDir(path: string): boolean {
+  let result
+
+  try {
+    result = lstatSync(path).isDirectory()
+  } catch (err) {
+    result = false
+  }
+
+  return result
+}
 
 /**
  * 拷贝目录
@@ -7,12 +23,16 @@ import { copy } from 'fs-extra'
  */
 export const ncpCopy = (what: string, where: string): Promise<void> => {
   return new Promise<void>((resolve, reject) => {
-    copy(what, where, err => {
-      if (err) {
-        console.log(err)
-        reject(err)
-      }
-      resolve()
-    })
+    if (isDir(what)) {
+      copy(what, where, err => {
+        if (err) {
+          console.log(err)
+          reject(err)
+        }
+        resolve()
+      })
+      return
+    }
+    reject('需要拷贝的目录不存在')
   })
 }
